80f8343e887b894dff1d88f569bb8c918c377bd8
Max Winokan <mwinokan@me.com>
2025-10-31 12:46:30 +0000

    ls data

--- run_knitwork.sh
+++ run_knitwork.sh
@@ -6,9 +6,9 @@ set -e # exit on any failed commands
 echo CMD $0 $@
 echo PWD $(pwd)
 echo LS $(ls -a)
+echo LS DATA $(ls -a data)
 echo LS PARENT $(ls -a ..)
 echo LS HOME $(ls -a $HOME)
-echo LS DATA $(ls -a /app/data)
 
 INSTANCE=$1
 echo INSTANCE $1
