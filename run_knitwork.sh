42baf2b8dd30aed2bc7db2b73e845e6e2d202e93
Max Winokan <mwinokan@me.com>
2025-11-06 09:15:47 +0000

    silent variables and make outputs visible in squonk

--- run_knitwork.sh
+++ run_knitwork.sh
@@ -3,41 +3,67 @@
 set -u # fail anytime unset variables are used
 set -e # exit on any failed commands
 
-# DATA_DIR=data
-
 LIGANDS=$@
 echo LIGANDS $LIGANDS
 
-CONFIG_PATH="knitwork_input/knitwork_config.json"
+OUTPUT_DIR=$(dirname "$1")
+echo OUTPUT_DIR $OUTPUT_DIR
 
-mkdir -pv knitwork_input
+CONFIG_PATH="knitwork_config.json"
 
-# configure graph location
-python -m knitwork configure GRAPH_LOCATION "bolt://"$NEO4J_LOCATION --config-path=$CONFIG_PATH
-python -m knitwork configure GRAPH_USERNAME $NEO4J_USERNAME --config-path=$CONFIG_PATH
-python -m knitwork configure GRAPH_PASSWORD $NEO4J_PASSWORD --config-path=$CONFIG_PATH
+mkdir -pv $OUTPUT_DIR/knitwork_input
 
-# combine individual sdfs into a single input
-python -m knitwork combine-inputs --root data $LIGANDS knitwork_input/input.sdf
+### configure variables
 
-# run fragmentation on input.sdf
-python -m knitwork fragment knitwork_input/input.sdf --config-path=$CONFIG_PATH
+python -m knitwork configure \
+	GRAPH_LOCATION "bolt://"$NEO4J_LOCATION \
+	--config-path=$CONFIG_PATH --silent
 
-# outputs: 
-# - fragment_output/molecules.pkl.gz
-# - fragment_output/molecules.sdf
-# - fragment_output/pairs.pkl.gz
+python -m knitwork configure \
+	GRAPH_USERNAME $NEO4J_USERNAME \
+	--config-path=$CONFIG_PATH --silent
 
-# run pure merging
-# python -m knitwork pure-merge --config-path=$CONFIG_PATH
+python -m knitwork configure \
+	GRAPH_PASSWORD $NEO4J_PASSWORD \
+	--config-path=$CONFIG_PATH --silent
 
-# outputs: 
-# - knitwork_output/pure_merges.pkl.gz
-# - knitwork_output/pure_merges.sdf
+### combine individual sdfs into a single input
 
-# run impure merging
-# python -m knitwork impure-merge --config-path=$CONFIG_PATH
+python -m knitwork combine-inputs \
+	--root=data \
+	$LIGANDS \
+	$OUTPUT_DIR/knitwork_input/input.sdf
 
-# outputs: 
-# - knitwork_output/impure_merges.pkl.gz
-# - knitwork_output/impure_merges.sdf
+### run fragmentation on input.sdf
+
+python -m knitwork fragment \
+	$OUTPUT_DIR/knitwork_input/input.sdf \
+	--config-path=$CONFIG_PATH \
+	--output-dir=$OUTPUT_DIR/fragment_output
+
+	# outputs: 
+	# - fragment_output/molecules.pkl.gz
+	# - fragment_output/molecules.sdf
+	# - fragment_output/pairs.pkl.gz
+
+### run pure merging
+
+python -m knitwork pure-merge \
+	--config-path=$CONFIG_PATH \
+	--fragment-dir=$OUTPUT_DIR/fragment_output \
+	--output-dir=$OUTPUT_DIR/knitwork_output
+
+	# outputs: 
+	# - knitwork_output/pure_merges.pkl.gz
+	# - knitwork_output/pure_merges.sdf
+
+### run impure merging
+
+python -m knitwork impure-merge \
+	--config-path=$CONFIG_PATH \
+	--fragment-dir=$OUTPUT_DIR/fragment_output \
+	--output-dir=$OUTPUT_DIR/knitwork_output
+
+	# outputs: 
+	# - knitwork_output/impure_merges.pkl.gz
+	# - knitwork_output/impure_merges.sdf
